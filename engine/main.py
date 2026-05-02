b42ef73654adf304eff1f805962d120a16a7e9c7
Jangga_MKS <jundierlangga@gmail.com>
2026-05-02 16:59:36 +0700

    fix(dockercompose):revisi docker compose di root agar engine dan blockchain bisa nyambung

--- engine/main.py
+++ engine/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-BLOCKCHAIN_GATEWAY_URL = os.getenv("BLOCKCHAIN_URL", "http://blockchain:3000")
+BLOCKCHAIN_GATEWAY_URL = os.getenv("BLOCKCHAIN_URL", "http://symbio-link-blockchain:3000")
 
 class SymbiosisRequest(BaseModel):
     sender_factory_id: str = Field(..., description="ID unik pabrik pengirim limbah")
